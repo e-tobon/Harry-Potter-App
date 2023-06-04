c5b7fb6ebb90faf3abd7c5b0a1fa6466f60dd1d7
Edgarunam <etobon96@gmail.com>
2023-06-04 01:14:50 -0600

    Description Profesor 30%

--- app/src/main/java/com/ets/harrypotter/ui/profesores/ProfDetailFragment.kt
+++ app/src/main/java/com/ets/harrypotter/ui/profesores/ProfDetailFragment.kt
@@ -5,8 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.view.marginTop
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import com.bumptech.glide.Glide
 import com.ets.harrypotter.R
 import com.ets.harrypotter.databinding.FragmentProfDetailBinding
 import com.ets.harrypotter.presentation.MainViewModel
@@ -23,7 +25,12 @@ class ProfDetailFragment : Fragment(R.layout.fragment_prof_detail) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentProfDetailBinding.bind(view)
-        Toast.makeText(requireContext(),"${mainViewModel.profesor.value}",Toast.LENGTH_SHORT).show()
+        binding.tvEspecie.text = mainViewModel.profesor.value?.species.toString()
+        binding.tvNombreResult.text = mainViewModel.profesor.value?.name.toString()
+        Glide.with(requireContext()).load(mainViewModel.profesor.value?.image)
+            .into(binding.ivImageResult)
+
+
     }
 
 }
