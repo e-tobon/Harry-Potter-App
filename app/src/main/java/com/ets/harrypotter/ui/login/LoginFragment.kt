67c85f59faa42bd4420a53a2e398467a7c8bafdc
Edgarunam <etobon96@gmail.com>
2023-06-06 00:20:14 -0600

    Registro auth terminado

--- app/src/main/java/com/ets/harrypotter/ui/login/LoginFragment.kt
+++ app/src/main/java/com/ets/harrypotter/ui/login/LoginFragment.kt
@@ -1,30 +1,63 @@
 package com.ets.harrypotter.ui.login
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.ets.harrypotter.R
 import com.ets.harrypotter.databinding.FragmentLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 
 class LoginFragment : Fragment(R.layout.fragment_login) {
 
+    private lateinit var auth: FirebaseAuth
+
     lateinit var binding: FragmentLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        auth = Firebase.auth
+
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentLoginBinding.bind(view)
         binding.btnLogin.setOnClickListener {
-            findNavController().navigate(R.id.action_loginFragment_to_selectionFragment)
+            val email = binding.etEmail.text.toString()
+            val password = binding.etPassword.text.toString()
+
+            if(email!!.isEmpty()){
+                Toast.makeText(requireContext(), "Ingrese su información", Toast.LENGTH_SHORT).show()
+            }
+
+
+            if(!email!!.isEmpty() and !password.isEmpty()){
+                loginEmai(email,password)
+            }
+
+
+
         }
         binding.tvCrearCuenta.setOnClickListener {
             findNavController().navigate(R.id.action_loginFragment_to_registroFragment)
         }
+    }
+
+    private fun loginEmai( email:String, password:String){
+
+            auth.signInWithEmailAndPassword(email,password)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful){
+                        val user = auth.currentUser
+                        findNavController().navigate(R.id.action_loginFragment_to_selectionFragment)
+                    }else{
+                        Toast.makeText(requireContext(),"Ha ocurrido un problema, cuenta invalida. Verifique sus datos o cree una nueva",Toast.LENGTH_LONG).show()
+                    }
+                }
     }
 }
--- app/src/main/java/com/ets/harrypotter/ui/login/RegistroFragment.kt
+++ app/src/main/java/com/ets/harrypotter/ui/login/RegistroFragment.kt
@@ -1,29 +1,73 @@
 package com.ets.harrypotter.ui.login
 
 import android.os.Bundle
-import android.view.LayoutInflater
+import android.util.Patterns
 import android.view.View
-import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.ets.harrypotter.R
 import com.ets.harrypotter.databinding.FragmentRegistroBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 
 class RegistroFragment : Fragment(R.layout.fragment_registro) {
 
+    private lateinit var auth: FirebaseAuth
     lateinit var binding:FragmentRegistroBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        auth = Firebase.auth
     }
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentRegistroBinding.bind(view)
+
+
+
+
         binding.btnRegistro.setOnClickListener{
-            findNavController().navigate(R.id.action_registroFragment_to_loginFragment)
+
+            val email = binding.etEmailRegister.text.toString()
+            val password = binding.etPasswordRegister.text.toString()
+            val userName = binding.etUserName.text.toString()
+
+            if (!email!!.isEmpty() and  !password!!.isEmpty() ){
+                registroUsuario(userName,email,password)
+            }
+            else{
+                Toast.makeText(requireContext(),"${email},${password}",Toast.LENGTH_LONG).show()
+            }
+
         }
+
+    }
+
+
+    private fun registroUsuario(userName:String,email:String,password:String){
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(requireActivity()) { task ->
+                if (task.isSuccessful) {
+                    val user = auth.currentUser
+                    Toast.makeText(
+                        requireContext(),
+                        "El usario " + userName + " creado con exito!", Toast.LENGTH_SHORT
+                    ).show()
+                    findNavController().navigate(R.id.action_registroFragment_to_loginFragment)
+
+                } else {
+                    Toast.makeText(
+                        requireContext(),
+                        getString(R.string.errorRegistro),
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+
     }
 
 }
